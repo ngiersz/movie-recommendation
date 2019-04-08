fd9d9b502d3aeae62ed5329f090dbc29920d578d
Natalia Gierszewska <ngiersz@gmail.com>
2019-04-08 18:34:54 +0200

    Rename variables and functions, some minor changes

--- engine/api_logic.py
+++ engine/api_logic.py
@@ -12,6 +12,9 @@ class RatingsClient:
         print(self.ratings.head())
         print('client init')
 
+    def create_ratings(self):
+        return self.create_merged_ratings_and_genres().sum()
+
     def create_merged_ratings_and_genres(self):
         df_user_rated = pd.read_csv('data/user_ratedmovies.dat', sep='\t', nrows=100)
         df_movie_genres = pd.read_csv('data/movie_genres.dat', sep='\t')
@@ -21,9 +24,6 @@ class RatingsClient:
             columns=['date_day', 'date_month', 'date_year', 'date_hour', 'date_minute', 'date_second'])
         ratings_multi_hot = ratings_multi_hot.groupby(['userID', 'movieID', 'rating'])
         return ratings_multi_hot
-
-    def create_ratings(self):
-        return self.create_merged_ratings_and_genres().sum()
 
     def get_ratings_and_genres_column_names(self):
         ratings = self.create_ratings()
@@ -38,17 +38,16 @@ class RatingsClient:
         return genres_column_names
 
     def get_ratings_json(self):
-        df_ratings = self.ratings.reset_index()
-        return df_ratings.to_json(orient='index')
+        return self.ratings.to_json(orient='index')
 
     def delete_rating(self):
         self.ratings = self.ratings.iloc[0:0]
         print('after del: ' + str(self.ratings.size))
         print(self.ratings.head())
 
-    def add_to_ratings(self, rating):
+    def add_json_to_ratings(self, rating):
         rating_series = pd.Series(json.loads(rating))
-        self.ratings = self.ratings.reset_index()
+        self.ratings = self.ratings
         # print(self.ratings)
         # print('before add: ' + str(self.ratings.shape))
         self.ratings = self.ratings.append(rating_series, ignore_index=True)
@@ -91,66 +90,48 @@ class RatingsClient:
     def get_df_from_list_of_dict(self, list):
         return pd.DataFrame.from_dict(list)
 
-    # zad 5
     def get_avg_ratings_for_genres(self):
         genres = self.get_genres_column_names(self.ratings)
-        DFAllUsersRatedMoviesWithMovieGenres = pd.DataFrame()
+        all_ratings = pd.DataFrame()
         for genre in genres:
             genre_ratings = self.ratings[self.ratings[genre] == 1]
-            DFAllUsersRatedMoviesWithMovieGenres = DFAllUsersRatedMoviesWithMovieGenres.append(
+            all_ratings = all_ratings.append(
                 pd.Series(genre_ratings.rating.mean(), index=[genre]), ignore_index=True)
-        print('all users:')
-        print(DFAllUsersRatedMoviesWithMovieGenres.sum())
-        return DFAllUsersRatedMoviesWithMovieGenres.sum()
+        # print('all users:')
+        # print(all_ratings.sum())
+        return all_ratings.sum()
 
-    # def is_genre_times_rating(self, row, genres):
-    #     genres_ratings = pd.Series(index=genres)
-    #     for genre in genres:
-    #         genres_ratings[genre] = row.rating * row.genre
-
-    # # zad 5
-    # def get_ratings_for_all_movies_unbiased(self):
-    #     ratings_and_genres = self.create_merged_ratings_and_genres()
-    #     print(ratings_and_genres.head())
-    #     genres = self.get_genres_column_names(ratings_and_genres)
-    #     ratings_and_genres.apply(self.is_genre_times_rating(genres), axis=1)
-    #     print(ratings_and_genres.head())
-    #     return ratings_and_genres
-
-    # zad 6
     def get_avg_user_ratings_for_genres(self, user_id):
         genres = self.get_genres_column_names(self.ratings)
         user_ratings = self.ratings[self.ratings.userID == user_id]
-        DFUserRatedMoviesWithMovieGenres = pd.DataFrame()
+        user_ratings_with_genres = pd.DataFrame()
         for genre in genres:
             genre_ratings = user_ratings[user_ratings[genre] == 1]
-            DFUserRatedMoviesWithMovieGenres = DFUserRatedMoviesWithMovieGenres.append(
+            user_ratings_with_genres = user_ratings_with_genres.append(
                 pd.Series(genre_ratings.rating.mean(), index=[genre]), ignore_index=True)
-        DFUserRatedMoviesWithMovieGenres = DFUserRatedMoviesWithMovieGenres.sum()
-        print('biased: ')
-        print(DFUserRatedMoviesWithMovieGenres)
-        return DFUserRatedMoviesWithMovieGenres
+        user_ratings_with_genres = user_ratings_with_genres.sum()
+        # print('biased: ')
+        # print(user_ratings_with_genres)
+        return user_ratings_with_genres
 
-    # zad 6
-    def DFUserRatedMoviesWithMovieGenres_unbiased(self, user_id):
+    def get_user_profile_unbiased(self, user_id):
         return self.get_avg_ratings_for_genres() - self.get_avg_user_ratings_for_genres(user_id)
 
 
 if __name__ == "__main__":
     # create_ratings_json()
-    print('client main')
     cl = RatingsClient()
     # print(cl.get_ratings_and_genres_column_names()[1])
 
     #  zad 4
-    list_of_dict = cl.get_list_of_dict(cl.ratings)
+    # list_of_dict = cl.get_list_of_dict(cl.ratings)
     # print(l.__getitem__(1))
     # data_check = cl.get_df_from_list_of_dict(list_of_dict)
     # print(cl.ratings.shape)
     # print(data_check.shape)
 
-    unbiased = cl.DFUserRatedMoviesWithMovieGenres_unbiased(75)
-    print('unbiased:')
-    print(unbiased)
+    # unbiased = cl.get_user_profile_unbiased(75)
+    # print('unbiased:')
+    # print(unbiased)
 
-    # print(cl.get_ratings_for_all_movies_unbiased().head())
+    print(cl.get_ratings_json())
